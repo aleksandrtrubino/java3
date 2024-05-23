5d0874fe295b24cdab1b799c91890eac8c227b02
aleksandrtrubino <trubino2003@gmail.com>
2024-05-23 17:15:33 +0300

    Добавил javadoc

--- src/main/java/ru/trubino/Main.java
+++ src/main/java/ru/trubino/Main.java
@@ -5,7 +5,17 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.function.BiConsumer;
 
+/**
+ * The Main class measures the performance of various operations on different types of List implementations.
+ */
 public class Main {
+
+    /**
+     * Measures the performance of add, get, and remove operations on a given list.
+     *
+     * @param list the list to be tested
+     * @param iterationNum the number of iterations to perform
+     */
     private static void measurePerformance(List<Integer> list, int iterationNum) {
         long addTime = measureTime(list, iterationNum, List::add);
         long getTime = measureTime(list, iterationNum, List::get);
@@ -16,6 +26,14 @@ public class Main {
         System.out.println(list.getClass().getSimpleName() + ":remove\t" + iterationNum + "\t\t" + deleteTime);
     }
 
+    /**
+     * Measures the time taken to perform a specified action on a list for a given number of iterations.
+     *
+     * @param list the list on which the action is performed
+     * @param iterations the number of iterations to perform
+     * @param action the action to be performed
+     * @return the time taken in nanoseconds
+     */
     private static long measureTime(List<Integer> list, int iterations, BiConsumer<List<Integer>, Integer> action) {
         long startTime = System.nanoTime();
         for (int i = 0; i < iterations; i++) {
@@ -24,6 +42,11 @@ public class Main {
         return System.nanoTime() - startTime;
     }
 
+    /**
+     * The main method that sets up the lists and calls the measurePerformance method for each list.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         final int ITERATION_NUM = 1000;
 
